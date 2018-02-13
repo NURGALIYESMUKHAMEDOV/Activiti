c2e408eaf8fbeda1e230de4a42e2cd0122738ae1
Nurgali.Yesmukhamedo <nurgali.yesmukhamedov@bsbnb.kz>
2018-02-13 19:00:37 +0600

    1

--- modules/activiti-webapp-rest2/src/main/java/org/activiti/rest/servlet/WebConfigurer.java
+++ modules/activiti-webapp-rest2/src/main/java/org/activiti/rest/servlet/WebConfigurer.java
@@ -2,7 +2,9 @@ package org.activiti.rest.servlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -13,8 +15,27 @@ public class WebConfigurer implements ServletContextListener {
 
     private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
 
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
+    public AnnotationConfigWebApplicationContext context;
+
+    public void setContext(AnnotationConfigWebApplicationContext context){
+        this.context = context;
+    }
+
+    public void contextInitialized(ServletContextEvent sce) {
+
+        ServletContext servletContext = sce.getServletContext();
+
         log.debug("Configuring Spring root application context");
+
+        AnnotationConfigWebApplicationContext rootContext = null;
+
+        if(context == null){
+            rootContext = new AnnotationConfigWebApplicationContext();
+            rootContext.refresh();
+        }else{
+            rootContext = context;
+        }
+
 
         log.debug("Web application fully configured");
     }
